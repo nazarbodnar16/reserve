3321dd0c2ebd5451aa6ecbbe4cf14e0734a6931c
AlexanderDryapko <alexander.driapko@gmail.com>
2018-10-19 14:17:24 +0300

    Comment form implementation, share button realization, trust engine page implementation.

--- tmp-trust-engine.php
+++ tmp-trust-engine.php
@@ -8,11 +8,10 @@ get_header(); ?>
 		<div class="container">
 			<h1 class="h1">TRUST ENGINE™</h1>
 			<div class="engine-video">
-				<!-- <img src="<?php echo get_template_directory_uri(); ?>/images/video.gif" alt=""> -->
-				<video autoplay loop src="<?php echo get_template_directory_uri(); ?>/images/video.mp4"></video>
+				<video autoplay loop src="<?php echo get_field('add_video'); ?>"></video>
 			</div>
 			<div class="trust-description">
-				<p>Our Trust Engine empowers you to automate your clients’ payments, reduce costs, generate customized online reports, and enhance the reliability and completion of payments</p>
+				<p><?php echo get_field('text_under_video'); ?></p>
 			</div>
 		</div>
 	</div>
@@ -20,113 +19,80 @@ get_header(); ?>
 		<div class="secure-logo bg-img"><img src="<?php echo get_template_directory_uri(); ?>/images/green-logo.jpg" alt="image description"></div>
 		<h2 class="h2">SECURE PAYMENTS</h2>
 		<ul class="payments-list">
-			<li class="payments-list--item">
-				<div class="img">
-					<img src="<?php echo get_template_directory_uri(); ?>/images/1.png" alt="">
-				</div>
-				<h3 class="h3">B2B<strong>SERVICES</strong></h3>
-			</li>
-			<li class="payments-list--item">
-				<div class="img">
-					<img src="<?php echo get_template_directory_uri(); ?>/images/2.png" alt="">
-				</div>
-				<h3 class="h3">IMPORT<strong>EXPORT</strong></h3>
-			</li>
-			<li class="payments-list--item">
-				<div class="img">
-					<img src="<?php echo get_template_directory_uri(); ?>/images/3.png" alt="">
-				</div>
-				<h3 class="h3">E-COM<strong>MARKETS</strong></h3>
-			</li>
-			<li class="payments-list--item">
-				<div class="img">
-					<img src="<?php echo get_template_directory_uri(); ?>/images/4.png" alt="">
-				</div>
-				<h3 class="h3">SUPPLY<strong>CHAINS</strong></h3>
-			</li>
+
+			<?php
+			// check if the repeater field has rows of data
+			if( have_rows('secure_payments_item') ):
+			 	// loop through the rows of data
+			    while ( have_rows('secure_payments_item') ) : the_row(); ?>
+
+			    		<li class="payments-list--item">
+							<div class="img">
+								<img src="<?php the_sub_field('icon'); ?>" alt="icon">
+							</div>
+							<h3 class="h3"><?php the_sub_field('text_under_icon'); ?></h3>
+						</li>
+
+			<?php endwhile;
+			else :
+			    // no rows found
+			endif;
+			?>
+
 		</ul>
 	</div>
 	<div class="features">
 		<div class="secure-logo bg-img"><img src="<?php echo get_template_directory_uri(); ?>/images/green-logo.jpg" alt="image description"></div>
 		<h2 class="h2">Feautures</h2>
 		<ul class="secure-list">
-			<li class="secure-list--item">
-				<div class="img">
-					<img src="<?php echo get_template_directory_uri(); ?>/images/5.png" alt="">
-				</div>
-				<div class="detail">
-					<h3 class="h3">PAYMENTS</h3>
-					<p>Unique, virtual account numbers for collection purposes, allow you to match each payment quickly and easily to a specific vendor, merchant or underlying customer.</p>
-				</div>
-			</li>
-			<li class="secure-list--item">
-				<div class="img">
-					<img src="<?php echo get_template_directory_uri(); ?>/images/6.png" alt="">
-				</div>
-				<div class="detail">
-					<h3 class="h3">CONTROLS</h3>
-					<p>Create certainty and reduce risk. Increase accuracy and limit exceptions. Approve payments or hold unauthorized transactions, eliminating errors and reducing fraud. </p>
-				</div>
-			</li>
-			<li class="secure-list--item">
-				<div class="img">
-					<img src="<?php echo get_template_directory_uri(); ?>/images/7.png" alt="">
-				</div>
-				<div class="detail">
-					<h3 class="h3">RECONCILATION</h3>
-					<p>Unique, virtual account numbers enhance and streamline the reconciliation process, allowing you to rapidly reconcile payments.</p>
-				</div>
-			</li>
-			<li class="secure-list--item">
-				<div class="img">
-					<img src="<?php echo get_template_directory_uri(); ?>/images/8.png" alt="">
-				</div>
-				<div class="detail">
-					<h3 class="h3">ANALYSIS</h3>
-					<p>Customizable, robust, detailed reporting allows you to track payments from inception to settlement and gain a better understanding of your clients.</p>
-				</div>
-			</li>
+			<?php
+			// check if the repeater field has rows of data
+			if( have_rows('feautures_items') ):
+			 	// loop through the rows of data
+			    while ( have_rows('feautures_items') ) : the_row(); ?>
+
+					<li class="secure-list--item">
+						<div class="img">
+							<img src="<?php the_sub_field('icon'); ?>" alt="icon">
+						</div>
+						<div class="detail">
+							<h3 class="h3"><?php the_sub_field('title'); ?></h3>
+							<p><?php the_sub_field('text_under_icon'); ?></p>
+						</div>
+					</li>
+
+			<?php endwhile;
+			else :
+			    // no rows found
+			endif;
+			?>
+
 		</ul>
 	</div>
 	<div class="features mod-benefits">
 		<h2 class="h2">BENEFITS</h2>
 		<ul class="secure-list">
-			<li class="secure-list--item">
-				<div class="img">
-					<img src="<?php echo get_template_directory_uri(); ?>/images/5.png" alt="">
-				</div>
-				<div class="detail">
-					<h3 class="h3">COMMAND</h3>
-					<p>Gain control of your cash flow, your customer’s experience and your payment data. Count on the outcome with tailored controls.</p>
-				</div>
-			</li>
-			<li class="secure-list--item">
-				<div class="img">
-					<img src="<?php echo get_template_directory_uri(); ?>/images/6.png" alt="">
-				</div>
-				<div class="detail">
-					<h3 class="h3">SIMPLE</h3>
-					<p>Create a system for tracking and controlling payments that matches your company’s needs, simplifying your payments.</p>
-				</div>
-			</li>
-			<li class="secure-list--item">
-				<div class="img">
-					<img src="<?php echo get_template_directory_uri(); ?>/images/7.png" alt="">
-				</div>
-				<div class="detail">
-					<h3 class="h3">SERVICE</h3>
-					<p>Our Trust Engineers are at the ready to help you quickly resolve exceptions, rapidly track and investigate problems, and deliver the answers you need.</p>
-				</div>
-			</li>
-			<li class="secure-list--item">
-				<div class="img">
-					<img src="<?php echo get_template_directory_uri(); ?>/images/8.png" alt="">
-				</div>
-				<div class="detail">
-					<h3 class="h3">GROWTH</h3>
-					<p>Our competitive pricing and feature rich offering reduces your costs and our unlimited funds flow capacity allows you to continue growing your customer base.</p>
-				</div>
-			</li>
+			<?php
+			// check if the repeater field has rows of data
+			if( have_rows('benefits_items') ):
+			 	// loop through the rows of data
+			    while ( have_rows('benefits_items') ) : the_row(); ?>
+
+					<li class="secure-list--item">
+						<div class="img">
+							<img src="<?php the_sub_field('icon'); ?>" alt="icon">
+						</div>
+						<div class="detail">
+							<h3 class="h3"><?php the_sub_field('title'); ?></h3>
+							<p><?php the_sub_field('text_under_icon'); ?></p>
+						</div>
+					</li>
+
+			<?php endwhile;
+			else :
+			    // no rows found
+			endif;
+			?>
 		</ul>
 	</div>
 </main>

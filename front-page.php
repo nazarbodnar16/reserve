72b146313cf6ab90e92052f35a787f52d1e2d208
nazar <nazarbodnar16@gmail.com>
2018-10-20 13:56:33 +0300

    add 2 section

--- front-page.php
+++ front-page.php
@@ -33,6 +33,36 @@ get_header(); ?>
 			<div class="text-box">
 				<p>Reserve Trust has partnered with the strongest and most trustworthy financial institutions to ensure that you always receive rapid, secure and cost-effective services.</p>
 			</div>
+			<ul class="partners-list">
+				<li class="li partners-list--item"><img src="<?php echo get_template_directory_uri(); ?>/images/partner-1.png" alt="image description"></li>
+				<li class="li partners-list--item"><img src="<?php echo get_template_directory_uri(); ?>/images/partner-2.png" alt="image description"></li>
+				<li class="li partners-list--item"><img src="<?php echo get_template_directory_uri(); ?>/images/partner-3.png" alt="image description"></li>
+			</ul>
+		</div>
+	</div>
+	<div class="standarts">
+		<div class="green-logo bg-img"><img src="<?php echo get_template_directory_uri(); ?>/images/green-logo.jpg" alt="image description"></div>
+		<div class="text-container">
+			<div class="heading">
+				<h2 class="h2">STANDARDS</h2>
+				<strong>Operate in a rich, fully regulated financial ecosystem.</strong>
+			</div>
+			<ul class="standarts-list">
+				<li class="standarts-list--item">
+					<div class="img">
+						<img src="<?php echo get_template_directory_uri(); ?>/images/standart-1.png" alt="image description">
+					</div>
+					<h3 class="h3">SAFETY AND SOUNDNESS</h3>
+					<p>The Reserve Trust Company is a Colorado chartered trust company, strictly regulated and carefully examined by the Colorado Division of Banking. We comply with federal banking laws and regulations. We are annually audited by a prominent CPA firm to ensure our systems, policies, procedures, and processes are industry leading. Your funds are always protected; they are held separately from our corporate assets and we do not make loans.</p>
+				</li>
+				<li class="standarts-list--item">
+					<div class="img">
+						<img src="<?php echo get_template_directory_uri(); ?>/images/standart-2.png" alt="image description">
+					</div>
+					<h3 class="h3">CYBER SECURITY</h3>
+					<p>The Reserve Trust Company conducts annual cybersecurity testing to protect our infrastructure and your funds. We utilize cloud data backup for redundancy, enhanced network security, industry leading encryption, and much more. We protect and respect your information. Our cybersecurity protocols exceed all regulatory and industry standards, including statutory and digital security requirements.</p>
+				</li>
+			</ul>
 		</div>
 	</div>	
 </main><!-- .site-main --><!-- .content-area -->

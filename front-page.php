f0c49659173b90893d4872944e4476ff7cb489a4
AlexanderDryapko <alexander.driapko@gmail.com>
2018-10-22 10:23:34 +0300

    Home page fixes

--- front-page.php
+++ front-page.php
@@ -29,14 +29,28 @@ get_header(); ?>
 	<div class="partners">
 		<div class="green-logo bg-img"><img src="<?php echo get_template_directory_uri(); ?>/images/green-logo.jpg" alt="image description"></div>
 		<div class="text-container">
-			<h2 class="h2">PARTNERS</h2>
+			<h2 class="h2"><?php echo get_field('partners_section_title'); ?></h2>
 			<div class="text-box">
-				<p>Reserve Trust has partnered with the strongest and most trustworthy financial institutions to ensure that you always receive rapid, secure and cost-effective services.</p>
+				<p><?php echo get_field('partners_section_subtitle'); ?></p>
 			</div>
+
 			<ul class="partners-list">
-				<li class="li partners-list--item"><img src="<?php echo get_template_directory_uri(); ?>/images/partner-1.png" alt="image description"></li>
-				<li class="li partners-list--item"><img src="<?php echo get_template_directory_uri(); ?>/images/partner-2.png" alt="image description"></li>
-				<li class="li partners-list--item"><img src="<?php echo get_template_directory_uri(); ?>/images/partner-3.png" alt="image description"></li>
+
+				<?php
+				// check if the repeater field has rows of data
+				if( have_rows('partners_logo') ):
+				 	// loop through the rows of data
+				    while ( have_rows('partners_logo') ) : the_row(); ?>
+
+						<li class="li partners-list--item"><img src="<?php the_sub_field('logo'); ?>" alt="Partners logo"></li>
+						  
+				<?  endwhile;
+
+				else :
+				    // no rows found
+				endif;
+				?>
+
 			</ul>
 		</div>
 	</div>
@@ -48,20 +62,28 @@ get_header(); ?>
 				<strong>Operate in a rich, fully regulated financial ecosystem.</strong>
 			</div>
 			<ul class="standarts-list">
-				<li class="standarts-list--item">
-					<div class="img">
-						<img src="<?php echo get_template_directory_uri(); ?>/images/standart-1.png" alt="image description">
-					</div>
-					<h3 class="h3">SAFETY AND SOUNDNESS</h3>
-					<p>The Reserve Trust Company is a Colorado chartered trust company, strictly regulated and carefully examined by the Colorado Division of Banking. We comply with federal banking laws and regulations. We are annually audited by a prominent CPA firm to ensure our systems, policies, procedures, and processes are industry leading. Your funds are always protected; they are held separately from our corporate assets and we do not make loans.</p>
-				</li>
-				<li class="standarts-list--item">
-					<div class="img">
-						<img src="<?php echo get_template_directory_uri(); ?>/images/standart-2.png" alt="image description">
-					</div>
-					<h3 class="h3">CYBER SECURITY</h3>
-					<p>The Reserve Trust Company conducts annual cybersecurity testing to protect our infrastructure and your funds. We utilize cloud data backup for redundancy, enhanced network security, industry leading encryption, and much more. We protect and respect your information. Our cybersecurity protocols exceed all regulatory and industry standards, including statutory and digital security requirements.</p>
-				</li>
+
+				<?php
+					// check if the repeater field has rows of data
+					if( have_rows('standards_list') ):
+					 	// loop through the rows of data
+					    while ( have_rows('standards_list') ) : the_row(); ?>
+
+							<li class="standarts-list--item">
+								<div class="img">
+									<img src="<?php the_sub_field('icon'); ?>" alt="icon">
+								</div>
+								<h3 class="h3"><?php the_sub_field('subtitle'); ?></h3>
+								<p><?php the_sub_field('description'); ?></p>
+							</li>
+
+					<?  endwhile;
+
+					else :
+					    // no rows found
+					endif;
+				?>
+
 			</ul>
 		</div>
 	</div>	
